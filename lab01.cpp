4747f769fa9500363cd9e2e4f03cae8080349559
Xatnagh <darrenzou1221@gmail.com>
2020-01-28 17:22:18 -0500

    finished lab1

---
+++ lab01.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include<string>
+
+bool IsLower(char c){
+  return c>='a' && c<='z';
+}
+bool isUpper(char c){
+  return c>='A' && c<='Z';
+}
+
+int letterCount(std::string str){
+  return str.length();
+}
+std::string toLower(std::string str){
+  for(int i=0;i<str.length();i++){
+    if(isUpper(str[i])){
+      str[i]=32+int(str[i]);
+    }
+  }
+  return str;
+}
+std::string toUpper(std::string str){
+  for(int i=0;i<str.length();i++){
+    if(IsLower(str[i])){
+      str[i]=int(str[i])-32;
+    }
+  }
+  return str;
+}
+
+std::string Substr(std::string str,char s,char e){
+  std::string substring="";
+  int startindex=-1;
+  int endindex;
+  for(int i=0;i<str.length();i++){
+    if(str[i]==s){
+      startindex=i;
+    }
+    if(startindex!=-1 && str[i]==e){
+      return str.substr(startindex, i-1);
+    }
+  }
+  return "";
+}
+int main() {
+  using namespace std;
+  cout<<IsLower('d')<<endl;
+  cout<<toLower("DHAb")<<endl;
+  cout<<toUpper("bdsahdbsadT")<<endl;
+cout<<Substr("abcdefg", 'c', 'f');
+}
