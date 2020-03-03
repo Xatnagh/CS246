0ae43a15110ead05deda96c89abcf00aadde78d8
Xatnagh <darrenzou1221@gmail.com>
2020-03-03 16:41:43 -0500

    lab 10 down

--- lab10/lab10/lab08.cpp
+++ lab10/lab10/lab08.cpp
@@ -8,7 +8,7 @@ template <typename T>
 bool similar(ds::Node<T>* root1,ds::Node<T>* root2 ){
     ds::Node<T>* tmp1=root1;
     ds::Node<T>* tmp2=root2;
-    while(tmp1!=NULL||tmp2!=NULL){
+    while(tmp1!=NULL&&tmp2!=NULL){
         if(tmp1->GetData()!=tmp2->GetData()){
             return false;
 
